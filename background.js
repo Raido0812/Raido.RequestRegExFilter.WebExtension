bc2955c5a5f43a5c8a24575b3bd8a9defc8eaa32
Kuan-Nien Lu <kuannienlu@gmail.com>
2017-09-19 08:45:06 +0200

    Initial Experiment

---
+++ background.js
@@ -0,0 +1,40 @@
+﻿
+var currentBlacklist = [];
+function refreshBlacklist()
+{
+    browser.storage.local.get("regexBlacklist")
+        .then(function(storageResult){
+            currentBlacklist = storageResult.regexBlacklist || [];
+        }, function(error){
+            console.log(`Error: ${error}`);
+    });
+}
+
+
+function onLoad()
+{
+    refreshBlacklist();
+    function blockRequestIfBlackListed(requestDetails) {
+        var hasMatch = false;
+        var k = 0;
+        while(k<currentBlacklist.length && !hasMatch)
+        {
+            hasMatch = new RegExp(currentBlacklist[k]).test(requestDetails.url);
+            k++;
+        }
+        
+        if (hasMatch)
+        {
+            console.log("Canceling: " + requestDetails.url);
+            return {cancel: true};
+        }
+    }
+
+    browser.webRequest.onBeforeRequest.addListener(
+      blockRequestIfBlackListed,
+      {urls: ["<all_urls>"]},
+      ["blocking"]
+    );
+
+}
+document.addEventListener("DOMContentLoaded", onLoad);
---
+++ options.js
@@ -0,0 +1,91 @@
+﻿function onLoad()
+{
+    var blackListDropdown = document.querySelector("#blacklist")
+    
+    function reloadScriptDataOnExtension(){
+        browser.runtime.getBackgroundPage().then(function(page) {
+            page.refreshBlacklist();            
+        }, onError);
+    }
+    
+    function selectedRegExChanged(e){
+        // remove disabled
+        document.querySelector("#removeSelectedRegEx").removeAttribute('disabled');
+    }
+    
+    function removeSelectedRegEx(e){
+        browser.storage.local.get("regexBlacklist")
+            .then(function(storageResult){
+                //TODO: find item to remove and then save
+                var regExList = storageResult.regexBlacklist || [];
+                var indexOfSelectedItem = regExList.indexOf(blackListDropdown.value)
+                if (blackListDropdown.value &&
+                    indexOfSelectedItem > -1)
+                {
+                    regExList.splice(indexOfSelectedItem,1);
+                } 
+                
+                browser.storage.local.set({
+                  regexBlacklist: regExList
+                });
+                
+                setCurrentBlacklist(regExList);
+                reloadScriptDataOnExtension();
+            }, onError);
+    }
+
+    function addRegEx(e) {
+        e.preventDefault();
+      
+        browser.storage.local.get("regexBlacklist")
+            .then(function(storageResult){
+                var regexText = document.querySelector("#regexText");                
+                var regExList = storageResult.regexBlacklist || [];                
+                regExList.push(regexText.value);
+                
+                browser.storage.local.set({
+                  regexBlacklist: regExList
+                });
+                
+                setCurrentBlacklist(regExList);
+                reloadScriptDataOnExtension();
+            }, onError);
+    }
+    
+    function clearOptions(selectbox)
+    {
+        var i;
+        for(i = selectbox.options.length; i > 0 ; i--)
+        {
+            selectbox.remove(i-1);
+        }
+    }
+    
+    function setCurrentBlacklist(result) {
+        clearOptions(blackListDropdown);
+        if (Array.isArray(result))
+        {
+            // add regex as options
+            for (var i=0,l=result.length;i<l;i++)
+            {
+                var opt = document.createElement('option');
+                opt.value = result[i];
+                opt.text = result[i];
+                blackListDropdown.appendChild(opt);
+            }
+        }
+    }
+
+    function onError(error) {
+        console.log(`Error: ${error}`);
+    }
+
+    var getting = browser.storage.local.get("regexBlacklist");
+    getting.then(function(getResult){setCurrentBlacklist(getResult.regexBlacklist);}, onError);
+    
+    blackListDropdown.addEventListener("change", selectedRegExChanged);
+    document.querySelector("form").addEventListener("submit", addRegEx);
+    document.querySelector("#removeSelectedRegEx").addEventListener("click", removeSelectedRegEx);
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
